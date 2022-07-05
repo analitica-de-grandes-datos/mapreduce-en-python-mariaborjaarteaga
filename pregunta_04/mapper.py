6ad06c4e93b720d085095ce1f194894857079c70
Maria Jose Borja Arteaga <mborjaa@unal.edu.co>
2022-07-04 21:34:42 -0500

    pregunta 4
    pregunta 4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -4,5 +4,5 @@
 import sys
 if __name__ == "__main__":
     for line in sys.stdin:
-        columnas = line.split(' ')
-        sys.stdout.write("{}\t1\n".format(columnas[0]))
+        columnas = line.split(" ")
+        sys.stdout.write("{},1\n".format(columnas[0]))
