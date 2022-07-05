b07eca21de674b5761d447580315c1541838c02e
Maria Jose Borja Arteaga <mborjaa@unal.edu.co>
2022-07-04 21:52:09 -0500

    pregunta 6
    pregunta 6

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     total = 0
     for line in sys.stdin:
         key, val = line.split("\t")
-        val = int(val)
+        val = float(val)
         if key == ckey:
             if val > total:
                 total = val
