6dced7fcce14314dd13038ec8c7f31baed205f45
Maria Jose Borja Arteaga <mborjaa@unal.edu.co>
2022-07-04 20:09:11 -0500

    PRUEBA PREGUNTA 1
    PRUEBA PREGUNTA 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        #se separan las columnas separadas 
+        lista = line.split(',')
+        #se genera la salida del dato llamando la columna objetivo
+        sys.stdout.write("{}\t1\n".format(lista[2]))
