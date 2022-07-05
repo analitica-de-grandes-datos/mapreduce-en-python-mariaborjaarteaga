1d7590f17a3e5e69c7422bdcb5a5cf9f6886bc10
Maria Jose Borja Arteaga <mborjaa@unal.edu.co>
2022-07-04 22:22:43 -0500

    pregunta 6
    pregunta 6

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -4,5 +4,5 @@
 import sys
 if __name__ == "__main__":
     for line in sys.stdin:
-        columnas = line.split(',')
+        columnas = line.split('  ')
         sys.stdout.write("{}\t{}\n".format(columnas[0], float(columnas[2])))
