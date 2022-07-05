c6429f4f3a1ea62e11a0d72c5dd9732ac17b4477
Maria Jose Borja Arteaga <mborjaa@unal.edu.co>
2022-07-04 20:35:51 -0500

    pregunta 1
    pregunta 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -4,7 +4,5 @@
 import sys
 if __name__ == "__main__":
     for line in sys.stdin:
-        #se separan las columnas separadas 
-        lista = line.split(',')
-        #se genera la salida del dato llamando la columna objetivo
-        sys.stdout.write("{}\t1\n".format(lista[2]))
+        columnas = line.split(',')
+        sys.stdout.write("{}\t1\n".format(columnas[2]))
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -8,36 +8,17 @@ import sys
 #
 if __name__ == '__main__':
 
-    curkey = None
+    ckey = None
     total = 0
-
-    #
-    # cada linea de texto recibida es una entrada clave \tabulador valor
-    #
     for line in sys.stdin:
-
         key, val = line.split("\t")
         val = int(val)
-
-        if key == curkey:
-            #
-            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
-            # clave.
-            #
+        if key == ckey:
             total += val
         else:
-            #
-            # Se cambio de clave. Se reinicia el acumulador.
-            #
-            if curkey is not None:
-                #
-                # una vez se han reducido todos los elementos
-                # con la misma clave se imprime el resultado en
-                # el flujo de salida
-                #
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-            curkey = key
+            if ckey is not None:
+                sys.stdout.write("{}\t{}\n".format(ckey, total))
+            ckey = key
             total = val
 
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    sys.stdout.write("{}\t{}\n".format(ckey, total))
